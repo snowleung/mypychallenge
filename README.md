0113f657f192f66dbd738115adc45ea4a32a28b8
Sam <sliang@xtremeprog.com>
2015-07-06 11:33:41 +0800

    L1 tips see README L1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mypychallenge
 let's rock!!!!
+
+
+###L1
+* the tips is also an trans table too.
--- l1.py
+++ l1.py
@@ -4,21 +4,35 @@
 from http://www.pythonchallenge.com/pc/def/map.html
 '''
 
+VAL = ord('M') - ord('K')
+text = '''g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'''
 
-def solutions1():
+
+def solutions2():
     '''
-    K -> M
-    O -> Q
-    E -> G
+    twice
     '''
-    text = '''g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'''
-    VAL = ord('M') - ord('K')
-    output = ''.join([chr(ord(tt)+VAL) for tt in text])
-    print output
+    output = ''.join(reduce(_select, list(text), []))  # tips, an trans table also.
+    return output
 
 
-def encrytpe():
-    pass
+def _select(l, char):
+    if 'y' == char:
+        l.append('a')  # { -> a
+    elif 'z' == char:
+        l.append('b')  # { -> b
+    elif ord('a') <= ord(char) <= ord('z'):
+        l.append(chr(ord(char) + VAL))
+    else:
+        l.append(char)
+    return l
+
+
+def trans_map(s='map'):
+    import string
+    tb = string.maketrans(text, solutions2())
+    print string.translate('map', tb)  # map -> ocr
+
 
 if __name__ == '__main__':
-    solutions1()
+    trans_map()
