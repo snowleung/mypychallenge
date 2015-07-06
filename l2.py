4856694b6244d05c987d22f60d6839d918632768
Sam <sliang@xtremeprog.com>
2015-07-06 12:25:38 +0800

    add answer

--- l2.py
+++ l2.py
@@ -11,7 +11,7 @@ SOURCE_TEXT = urllib2.urlopen('''http://www.pythonchallenge.com/pc/def/ocr.html'
 def find_content():
     source = re.findall(r'<!--.*?-->', (''.join(SOURCE_TEXT.readlines())).replace('\n', ''))[1]
     txt = re.findall(r'[a-zA-Z]', source)
-    print ''.join(txt)
+    print ''.join(txt)          # equality
 
 if __name__ == '__main__':
     find_content()
